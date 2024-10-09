c0a8ecd6fa3baceca277cf71c1e208f8d7bd9df3
I.Mihara <issei.mihara@xforestx.com>
2024-10-09 13:49:43 +0000

    Refactor runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,6 @@
 using Demux
 using Test
 
-const demo_FASTQ_file1 = "demo_file1.fastq"
-const demo_FASTQ_file2 = "demo_file2.fastq"
-const demo_FASTQ_file1_mutated = "demo_file1_mutated.fastq"
-const demo_FASTQ_file2_mutated = "demo_file2_mutated.fastq"
-const demo_bc_file = "demo_bc_file.csv"
-
 function check_output_files(output_dir::String, ideal_dir::String)
     ideal_files = readdir(ideal_dir)
 
@@ -25,57 +19,74 @@ end
 @testset "Demux.jl" begin
     @testset "Single thread with 1 FASTQ file" begin
         mktempdir() do output_dir
-            ideal_dir = "path/to/ideal_outputs"
+            ideal_dir = "results/demo1_R1"
 
-            execute_demultiplexing(demo_FASTQ_file1, demo_bc_file, output_dir)
-            check_output_files_with_diff(output_dir, ideal_dir)
+            files = readdir("FASTQ_files/demo1_R1")
+            for file in files
+                execute_demultiplexing("FASTQ_files/demo1_R1/$file", "reference_files/demo1.tsv", "output_dir")
+            end
+            check_output_files(output_dir, ideal_dir)
         end
     end
 
     @testset "Single thread with 2 FASTQ files" begin
         mktempdir() do output_dir
-            ideal_dir = "path/to/ideal_outputs"
+            ideal_dir = "results/demo1_R2"
 
-            execute_demultiplexing(demo_FASTQ_file1, demo_FASTQ_file2, demo_bc_file, output_dir)
-            check_output_files_with_diff(output_dir, ideal_dir)
+            files_R1 = readdir("FASTQ_files/demo1_R1")
+            files_R2 = readdir("FASTQ_files/demo1_R2")
+            for (file_R1, file_R2) in zip(files_R1, files_R2)
+                execute_demultiplexing("FASTQ_files/demo1_R1/$file", "FASTQ_files/demo1_R2/$file", "reference_files/demo1.tsv", "output_dir")
+            end
+            check_output_files(output_dir, ideal_dir)
         end
     end
 
     @testset "Multi-thread with 1 FASTQ file" begin
         mktempdir() do output_dir
-            ideal_dir = "path/to/ideal_outputs"
+            ideal_dir = "results/demo1_R1"
 
             addprocs(4)
             @everywhere using Demux
-            execute_demultiplexing(demo_FASTQ_file1, demo_bc_file, output_dir)
-            check_output_files_with_diff(output_dir, ideal_dir)
+            files = readdir("FASTQ_files/demo1_R1")
+            for file in files
+                execute_demultiplexing("FASTQ_files/demo1_R1/$file", "reference_files/demo1.tsv", "output_dir")
+            end
+            check_output_files(output_dir, ideal_dir)
         end
         rmprocs(nworkers())
     end
 
     @testset "Multi-thread with 2 FASTQ files" begin
         mktempdir() do output_dir
-            ideal_dir = "path/to/ideal_outputs"
+            ideal_dir = "results/demo1_R2"
 
             addprocs(4)
             @everywhere using Demux
-            execute_demultiplexing(demo_FASTQ_file1, demo_FASTQ_file2, demo_bc_file, output_dir)
-            check_output_files_with_diff(output_dir, ideal_dir)
+            files_R1 = readdir("FASTQ_files/demo1_R1")
+            files_R2 = readdir("FASTQ_files/demo1_R2")
+            for (file_R1, file_R2) in zip(files_R1, files_R2)
+                execute_demultiplexing("FASTQ_files/demo1_R1/$file", "FASTQ_files/demo1_R2/$file", "reference_files/demo1.tsv", "output_dir")
+            end
+            check_output_files(output_dir, ideal_dir)
         end
         rmprocs(nworkers())
     end
 
     @testset "Multi-thread with 2 FASTQ files with options" begin
         mktempdir() do output_dir
-            ideal_dir = "path/to/ideal_outputs"
+            ideal_dir = "results/demo2"
 
-            addprocs(16)
+            addprocs(4)
             @everywhere using Demux
-            execute_demultiplexing(demo_FASTQ_file1_mutated, demo_FASTQ_file2_mutated, demo_bc_file, output_dir,
-                max_error_rate=0.1, min_delta=0.05, mismatch=2, indel=2, classify_both=true, bc_complement=true,
-                output_prefix1="test_prefix1", output_prefix2="test_prefix2")
-            
-            check_output_files_with_diff(output_dir, ideal_dir)
+            files_R1 = readdir("FASTQ_files/demo2_R1")
+            files_R2 = readdir("FASTQ_files/demo2_R2")
+            for (file_R1, file_R2) in zip(files_R1, files_R2)
+                execute_demultiplexing("FASTQ_files/demo2_R1/$file_R1", "FASTQ_files/demo2_R2/$file_R2", "reference_files/demo2.csv", "output_dir",
+                    max_error_rate=0.25, min_delta=0.15, mismatch=1, indel=2, classify_both=true, bc_complement=true,
+                    output_prefix="test_prefix1", output_prefix2="test_prefix2")
+            end
+            check_output_files(output_dir, ideal_dir)
         end
         rmprocs(nworkers())
     end
