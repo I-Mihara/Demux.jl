f55c0c5f8fb0381f166bfddfe27113def82d9337
I.Mihara <issei.mihara@xforestx.com>
2024-10-10 14:27:06 +0000

    Refactor runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -42,6 +42,22 @@ end
             check_output_files(output_dir, ideal_dir)
         end
     end
+    @testset "Single thread with 2 FASTQ files with options" begin
+        mktempdir() do output_dir
+            ideal_dir = "results/demo2"
+
+            files_R1 = readdir("FASTQ_files/demo2_R1")
+            files_R2 = readdir("FASTQ_files/demo2_R2")
+            for (file_R1, file_R2) in zip(files_R1, files_R2)
+                filename = replace(basename(file_R1), r"\.fastq.gz$" => "")
+                filename2 = replace(basename(file_R2), r"\.fastq.gz$" => "")
+                execute_demultiplexing("FASTQ_files/demo2_R1/$file_R1", "FASTQ_files/demo2_R2/$file_R2", "reference_files/demo2.csv", output_dir,
+                    max_error_rate=0.25, min_delta=0.15, mismatch=1, indel=2, classify_both=true, bc_complement=true, bc_rev=true,
+                    output_prefix1="test_prefix1." * filename, output_prefix2="test_prefix2." * filename2)
+            end
+            check_output_files(output_dir, ideal_dir)
+        end
+    end
 end
 
 addprocs(3)
@@ -67,23 +83,6 @@ addprocs(3)
             files_R2 = readdir("FASTQ_files/demo1_R2")
             for (file_R1, file_R2) in zip(files_R1, files_R2)
                 execute_demultiplexing("FASTQ_files/demo1_R1/$file_R1", "FASTQ_files/demo1_R2/$file_R2", "reference_files/demo1.tsv", output_dir)
-            end
-            check_output_files(output_dir, ideal_dir)
-        end
-    end
-    
-    @testset "Multi-thread with 2 FASTQ files with options" begin
-        mktempdir() do output_dir
-            ideal_dir = "results/demo2"
-    
-            files_R1 = readdir("FASTQ_files/demo2_R1")
-            files_R2 = readdir("FASTQ_files/demo2_R2")
-            for (file_R1, file_R2) in zip(files_R1, files_R2)
-                filename = replace(basename(file_R1), r"\.fastq.gz$" => "")
-                filename2 = replace(basename(file_R2), r"\.fastq.gz$" => "")
-                execute_demultiplexing("FASTQ_files/demo2_R1/$file_R1", "FASTQ_files/demo2_R2/$file_R2", "reference_files/demo2.csv", output_dir,
-                    max_error_rate=0.25, min_delta=0.15, mismatch=1, indel=2, classify_both=true, bc_complement=true, bc_rev=true,
-                    output_prefix1="test_prefix1." * filename, output_prefix2="test_prefix2." * filename2)
             end
             check_output_files(output_dir, ideal_dir)
         end
